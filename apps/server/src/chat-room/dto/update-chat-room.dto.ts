775941b7bbfa6f485997eff25a06d8cf2ee50b64
hrach.hovakimyan <hrach.hovakimyan@mil.pub>
2023-12-21 12:44:49 +0400

    fix(client): remove unused code

--- apps/server/src/chat-room/dto/update-chat-room.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateChatRoomDto } from './create-chat-room.dto';
-
-export class UpdateChatRoomDto extends PartialType(CreateChatRoomDto) {
-    id: number;
-}
